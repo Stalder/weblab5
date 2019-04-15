55b98c89d8aa2582770a0de580dd1d24365c6789
Sergei Lavinov <serg_lavinov@inbox.ru>
2019-04-15 17:54:24 +0300

    Removed redundant

--- database/index.js
+++ database/index.js
@@ -2,7 +2,6 @@ var mongoose = require("mongoose");
 var credits = require("../credentials");
 
 mongoose.connect(
-  //   "mongodb+srv://stalder:stalder525@lab5-bea1i.mongodb.net/test?retryWrites=true",
   `mongodb+srv://${credits.user}:${
     credits.password
   }@lab5-bea1i.mongodb.net/test?retryWrites=true`,
